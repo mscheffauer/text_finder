b63492e5387acd241efd422cbf2ab3df4c0252c3
Martin Scheffauer <59596935+mscheffauer@users.noreply.github.com>
2022-07-25 09:19:13 +0200

    Add files via upload

---
+++ src/build/cv/task1/CMakeFiles/cvtask1.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cvexpert/.ssh/cgcvmaster-cv-task-1-51917931/src/cv/task1/algorithms.cpp" "/home/cvexpert/.ssh/cgcvmaster-cv-task-1-51917931/src/build/cv/task1/CMakeFiles/cvtask1.dir/algorithms.cpp.o"
+  "/home/cvexpert/.ssh/cgcvmaster-cv-task-1-51917931/src/cv/task1/helper.cpp" "/home/cvexpert/.ssh/cgcvmaster-cv-task-1-51917931/src/build/cv/task1/CMakeFiles/cvtask1.dir/helper.cpp.o"
+  "/home/cvexpert/.ssh/cgcvmaster-cv-task-1-51917931/src/cv/task1/main.cpp" "/home/cvexpert/.ssh/cgcvmaster-cv-task-1-51917931/src/build/cv/task1/CMakeFiles/cvtask1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FULL_VERSION=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/software/opencv/include"
+  "../cgcvcommon"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/cv/task1/CMakeFiles/cvtask1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvtask1.dir/algorithms.cpp.o"
+  "CMakeFiles/cvtask1.dir/helper.cpp.o"
+  "CMakeFiles/cvtask1.dir/main.cpp.o"
+  "cvtask1"
+  "cvtask1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvtask1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
